12f76c911efd07a5c96a58d6e47109df7ecfdc48
Eric Davis <bericdavis42@gmail.com>
2021-12-01 17:41:02 -0500

    commit

--- 01/python/01a.py
+++ 01/python/01a.py
@@ -3,14 +3,13 @@
 increase = 0
 decrease = 0
 previous = 0
+# read in the file as an array
 with open('../../data/01/inputa.txt') as fin:
     input = fin.read().splitlines()[1:]
 
 for a in input:
-    print(a)
     if int(a) > previous:
         increase += 1
-        print('increased')
     if int(a) < previous:
         decrease += 1
     previous = int(a)
